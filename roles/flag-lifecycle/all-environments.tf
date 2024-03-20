08fa9154778d8761de3c3818d7321a9144c71cdb
Christopher Tarquini <ctarquini@launchdarkly.com>
2024-03-20 17:40:18 -0400

    refactor roles

--- roles/flag-lifecycle/all-environments.tf
+++ roles/flag-lifecycle/all-environments.tf
@@ -1,53 +1,77 @@
 
 output "project-roles"{
     description = "Project-level flag lifecycle management roles"
-    value = {
-        "view-project" = launchdarkly_custom_role.view-project,
-        "sdk-key" = launchdarkly_custom_role.sdk-key,
-        "flag-manager" = launchdarkly_custom_role.flag-manager,
-        "release-manager" = launchdarkly_custom_role.release-manager,
-        "flag-archiver" = launchdarkly_custom_role.flag-archiver,
-        "variation-manager" = launchdarkly_custom_role.variation-manager,
-        "sdk-manager" = launchdarkly_custom_role.sdk-manager
-    }
+    value = merge(
+    {
+        "view_project" = launchdarkly_custom_role.view_project,
+        "create_update_flags" = launchdarkly_custom_role.create_update_flags,
+        "archive_flags" = launchdarkly_custom_role.archive_flags,
+        "update_client_side_availability" = launchdarkly_custom_role.update_client_side_availability,
+    }, var.seperate_update_flag_variations_role ? {
+        "update_flag_variations" = launchdarkly_custom_role.update_flag_variations[0]
+    } : {})
 }
 
 /// Read only access to a subset of projects
 /// May create approval requests.
 /// Rationale: Empower any team to request changes to the application. May be scoped with a tag. Think requesting user impersonation
-resource "launchdarkly_custom_role" "view-project" {
-  key              = "view-${local.project.key}"
-  name             = "View - ${local.project.name}"
+resource "launchdarkly_custom_role" "view_project" {
+  key              = "view-project-${var.key}"
+  name             = "View Project - ${local.name}"
   description      = "Can view the project and its flags, but cannot make changes."
   base_permissions = "no_access"
   policy_statements {
     effect    = "allow"
-    resources = ["proj/${local.project.specifier}"]
-    actions   = ["viewProject"]
+    resources = local.include_project_specifiers
+    actions   = concat(["viewProject"], var.viewers_can_request_changes ? ["createApprovalRequest"] : [])
   }
-
-  policy_statements {
-    effect    = "allow"
-    resources = ["proj/${local.project.specifier}:env/*:flag/*"]
-    actions   = ["createApprovalRequest"]
-
+  dynamic "policy_statements" {
+    for_each = local.exclude_project_specifiers
+    content {
+      effect    = "deny"
+      resources = policy_statements.value
+      actions   = ["*"]
+    }
   }
 }
 
-resource "launchdarkly_custom_role" "flag-manager" {
-  key              = "flag-manager-${local.project.key}"
-  name             = "Flag Manager - ${local.project.name}"
-  description      = "May perform flag management actions that do not impact the evaluation of existing flags."
+resource "launchdarkly_custom_role" "create_update_flags" {
+  key              = "create-update-flags-${var.key}"
+  name             = "Create Flags / Update Metadata - ${local.name}"
+  description      = "Create and update global flag metadata. Can not perform actions that impact evaluation of existing flags"
   base_permissions = "no_access"
-
+  policy_statements {
+    effect    = "allow"
+    resources = local.include_project_specifiers
+    actions   = "viewProject"
+  }
   policy_statements {
     effect    = "allow"
     resources = ["proj/${local.project.specifier}:env/*:flag/*"]
-    actions   = ["cloneFlag", "createFlag", "createFlagLink", "deleteFlagLink", "updateDescription", "updateFlagCustomProperties", "updateFlagDefaultVariations", "updateFlagLink", "updateMaintainer", "updateName", "updateTags", "updateTemporary"]
+    actions   = concat([
+      /* 
+       *  Always include createApprovalRequest to prevent
+       *  unintended consequences if you remove it from view project
+       */
+        "createApprovalRequest",
+        "cloneFlag",
+        "createFlag",
+        "createFlagLink",
+        "deleteFlagLink",
+        "updateDescription",
+        "updateFlagCustomProperties",
+        "updateFlagDefaultVariations",
+        "updateFlagLink",
+        "updateMaintainer",
+        "updateName",
+        "updateTags",
+        "updateTemporary"
+      ], var.with_separate_variation_manager == false ? [
+        "updateFlagVariations"
+      ] : [])
   }
-
   dynamic "policy_statements" {
-    for_each = var.with_seperate_context_manager == false ? toset([{
+    for_each = var.seperate_manage_contexts_role == false ? toset([{
       effect    = "allow"
       resources = ["proj/${local.project.specifier}:context-kind/*"]
       actions   = ["createContextKind", "updateContextKind", "updateAvailabilityForExperiments"]
@@ -60,11 +84,16 @@ resource "launchdarkly_custom_role" "flag-manager" {
   }
 }
 
-resource "launchdarkly_custom_role" "context-manager" {
-  key              = "context-manager-${local.project.key}"
-  name             = "Context Manager - ${local.project.name}"
+resource "launchdarkly_custom_role" "manage_context_kinds" {
+  key              = "manage-context-kinds-${var.key}"
+  name             = "Manage Context Kinds - ${local.name}"
   description      = "May create and update context kinds"
   base_permissions = "no_access"
+  policy_statements {
+    effect    = "allow"
+    resources = ["proj/${local.project.specifier}"]
+    actions   = "viewProject"
+  }
   policy_statements {
     effect    = "allow"
     resources = ["proj/${local.project.specifier}:context-kind/*"]
@@ -72,12 +101,16 @@ resource "launchdarkly_custom_role" "context-manager" {
   }
 }
 
-resource "launchdarkly_custom_role" "flag-archiver" {
-  key              = "archiver-${local.project.key}"
-  name             = "Archiver - ${local.project.name}"
+resource "launchdarkly_custom_role" "archive_flags" {
+  key              = "archive-flags-${var.key}"
+  name             = "Archive Flags - ${local.name}"
   description      = "May archive flags but not delete them. This action impacts the evaluation of existing flags in all environments. It can, however, be easily undone."
   base_permissions = "no_access"
-
+  policy_statements {
+    effect    = "allow"
+    resources = ["proj/${local.project.specifier}"]
+    actions   = "viewProject"
+  }
   policy_statements {
     effect    = "allow"
     resources = ["proj/${local.project.specifier}:env/*:flag/*"]
@@ -85,9 +118,10 @@ resource "launchdarkly_custom_role" "flag-archiver" {
   }
 }
 
-resource "launchdarkly_custom_role" "variation-manager" {
-  key              = "varmgr-${local.project.key}"
-  name             = "Variation Manager - ${local.project.name}"
+resource launchdarkly_custom_role "update_flag_variations" {
+  count = var.with_separate_variation_manager ? 1 : 0
+  key              = "update-flag-variations-${var.key}"
+  name             = "Variation Manager - ${local.name}"
   description      = "Impacts evaluation of existing flags in all environments"
   base_permissions = "no_access"
 
@@ -98,12 +132,17 @@ resource "launchdarkly_custom_role" "variation-manager" {
   }
 }
 
-resource "launchdarkly_custom_role" "sdk-manager" {
-  key              = "sdk-mgr-${local.project.key}"
-  name             = "SDK Manager - ${local.project.name}"
+
+resource launchdarkly_custom_role "update_client_side_availability" {
+  key              = "update-flag-avail-${var.key}"
+  name             = "Update Flags - Client-side Availability - ${local.name}"
   description      = "Impacts the evaluation of flags in all environments for client-side SDKs"
   base_permissions = "no_access"
-
+  policy_statements {
+    effect    = "allow"
+    resources = ["proj/${local.project.specifier}"]
+    actions   = "viewProject"
+  }
   policy_statements {
     effect    = "allow"
     resources = ["proj/${local.project.specifier}:env/*:flag/*"]
@@ -111,5 +150,4 @@ resource "launchdarkly_custom_role" "sdk-manager" {
 
   }
 }
-
 
--- roles/flag-lifecycle/variables.tf
+++ roles/flag-lifecycle/variables.tf
@@ -1,66 +1,92 @@
-variable "project" {
-    type = object({
-        name = string
-        key  = string
-    })
-    description = "LaunchDarkly project. The key will be used to generate role keys. If you are using wildcards, use the project_specifier variable"
-}
 
-variable "role_key" {
-    type = string
-    description = "Value appended to generated role keys. If null, project.key will be used"
+
+variable "name" {
+    type = string 
+    description = "Suffix appended to role names, normally set to the project name. Defaults to title(key)"
     default = null
 }
 
-variable "with_seperate_context_manager" {
-    type = bool
-    description = "Whether to generate a role for context manager. If false, flag manager will have createContextKind, deleteContextKind, and updateContextKind permissions"
-    default = true
+
+variable "key" {
+    type = string
+    description = "Suffix appended to role keys, if null, will be set to the name_suffix in kebab-case"
+    nullable = false
+    validation {
+      /* must be a valid launchdarkly resource specifier component */
+        condition = regex("^[a-zA-Z0-9-_]+$", var.key)
+        error_message = "key must contain only alphanumeric characters, hyphens, and underscores"
+    }
 }
 
-variable "environments" {
+variable "include_projects" {
+    type = set(string)
+    description = "Specifier(s) used to target projects and project sub-resources. Can be `project-key`, `*;tag,tag`, `project-key-prefix-*`, etc"
+    nullable = false
+    validation {
+    /* must not be empty */
+        condition = length(var.include_projects) > 0
+        error_message = "include_projects must not be empty. Use [`*`] to include all projects"
+    }
+    validation {
+      /* must be a valid launchdarkly resource specifier component */
+        condition = alltrue([for p in var.include_projects: can(regex("^[a-zA-Z0-9-_*]+(?:;(?:[{}: a-zA-Z0-9-_*]+,?)+)?$", p))])
+        error_message = "include_projects must be list of valid launchdarkly resource specifier (examples: `*`, `my-project-key`, `*;tag1,tag2`)"
+    }
+}
+
+variable "exclude_projects" {
+    type = set(string)
+    description = "Specifier(s) used to exclude projects and project sub-resources. Can be `project-key`, `*;tag,tag`, `project-key-prefix-*`, etc"
+    default = set()
+    validation {
+      /* must be a valid launchdarkly resource specifier component */
+        condition = alltrue([for p in var.exclude_projects: can(regex("^[a-zA-Z0-9-_*]+(?:;(?:[{}: a-zA-Z0-9-_*]+,?)+)?$", p))])
+        error_message = "exclude_projects must be list of valid launchdarkly resource specifier (examples: `*`, `my-project-key`, `*;tag1,tag2``)"
+    }
+}
+
+variable "seperate_update_flag_variations_role" {
+    type = bool
+    description = "Whether to generate a seperate update_flag_variations role. If false, update flags role will have updateFlagVariations permission"
+    default = false
+}
+variable "seperate_manage_contexts_role" {
+    type = bool
+    description = "Whether to generate a role for managing contexts. If false, update flags will have createContextKind, deleteContextKind, and updateContextKind permissions"
+    default = false
+}
+
+variable "environment_role_groups" {
     type = map(object({
         name = string
-        key = string
+        includes = set(string)
+        excludes = optional(set(string), set())
     }))
 
   default = {
-    "all" = {
-      key = "*"
-      name = "All environments"
+    "non-critical" = {
+      includes = ["*;{critical:false}"]
+      name = "noncritical"
     }
-    "test" = {
-      key = "test"
-      name = "Test"
-    }
-    "production" = {
-      key = "productionf"
-      name = "Production"
+    "critical" = {
+      includes = ["*;{critical:true}"]
+      name = "Critical"
     }
   }
 }
 
-variable "environment_excludes" {
-    type = map(list(string))
-    description = "Map of environment keys to a list of environments to generate deny statements for when creating environment-levle roles"
-    default = {}
+
+
+variable "viewers_can_request_changes" {
+    type = bool
+    description = "When true, `createApprovalRequest` will be included in the view project role"
+    default = true
 }
 
-locals {
-    
-    project = {
-        key = var.role_key != null ? var.role_key : var.project.key
-        name = var.project.name
-        specifier = var.project.key
-    }
 
-    environments = {
-        for key,value in var.environments: key => {
-            name = value.name
-            specifier = value.key != null ? value.key : key
-        }
-    }
-    denied_environments = {
-        for k,v in local.environments : k => [for exclude in try(var.environment_excludes[k], []): local.environments[exclude]]
-    }
+locals {
+    // var.name or title-cased var.key with _ , .and - replaced with spaces 
+   name = coalesce(var.name, title(replace(var.key, "/[_.-]+/", " ")))
+   include_project_specifiers = toset([for p in var.include_projects: "proj/${p}"])
+   exclude_project_specifiers = toset([for p in var.exclude_projects: "proj/${p}"])
 }
